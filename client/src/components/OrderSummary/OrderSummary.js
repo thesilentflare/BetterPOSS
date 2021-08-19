6fd84d0f945f1512820b08387a74f4620d5f8a25
Gavin L <gavin.leung.cms@gmail.com>
2021-08-19 19:10:37 -0400

    order summary changes
    ui changes to order summary

--- client/src/components/OrderSummary/OrderSummary.js
+++ client/src/components/OrderSummary/OrderSummary.js
@@ -17,12 +17,43 @@ const temp = {
       amount: 1,
       price: 0.99,
     },
+    {
+      type: "Fries",
+      name: "FFries",
+      size: "S",
+      special: ["PROMO"],
+      amount: 1,
+      price: 0.0,
+    },
   ],
+  subtotal: 5.58,
+  tax: 0.73,
+  total: 6.31,
 };
 
 const OrderSummary = () => {
   return (
     <div className="container__order">
+      <div
+        style={{
+          borderBottom: "1px solid black",
+        }}
+      >
+        <div
+          style={{
+            display: "inline-grid",
+            gridTemplateColumns: "repeat(4, 30px [col-start])",
+            columnGap: "50px",
+            margin: "10px 10px 0px 30px",
+          }}
+        >
+          <div>Qt.</div>
+          <div>Item</div>
+          <div>Size</div>
+          <div>Price</div>
+        </div>
+      </div>
+
       <div>
         <div style={{ display: "flex", flexDirection: "column" }}>
           {temp.items.map((item, key) => {
@@ -31,37 +62,53 @@ const OrderSummary = () => {
                 <div
                   style={{
                     display: "inline-grid",
-                    gridTemplateColumns: "repeat(4, 40px [col-start])",
+                    gridTemplateColumns: "repeat(4, 30px [col-start])",
                     columnGap: "50px",
+                    margin: "10px 10px 0px 30px",
                   }}
                 >
                   <div>{item.amount}</div>
                   <div>{item.name}</div>
                   <div>{item.size ? item.size : " "}</div>
-                  <div>{item.price}</div>
+                  <div>{item.price.toFixed(2)}</div>
                 </div>
-                <div style={{ display: "flex", flexDirection: "column" }}>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    margin: "10px",
+                    marginTop: 0,
+                  }}
+                >
                   {/* {item.special ? item.special : " "} */}
-                  {item.special ? item.special.map((special) => {
-                    return (
-                      <div>
-                        <div>*{special}</div>
-                      </div>
-                    );
-                  }): " "}
+                  {item.special
+                    ? item.special.map((special) => {
+                        return (
+                          <div style={{marginLeft: "25px"}}>
+                            <div>*{special}</div>
+                          </div>
+                        );
+                      })
+                    : " "}
                 </div>
               </div>
             );
           })}
         </div>
-        <div>
-          SubTOTAL
-        </div>
-        <div>
-          TAX
-        </div>
-        <div>
-          TOTAL
+        <div
+          style={{
+            display: "inline-grid",
+            gridTemplateColumns: "repeat(2, 190px [col-start])",
+            columnGap: "50px",
+            margin: "10px 10px 0px 30px",
+          }}
+        >
+          <div>SubTOTAL:</div>
+          <div>{temp.subtotal.toFixed(2)}</div>
+          <div>TAX:</div>
+          <div>{temp.tax.toFixed(2)}</div>
+          <div>TOTAL:</div>
+          <div>{temp.total.toFixed(2)}</div>
         </div>
       </div>
     </div>
