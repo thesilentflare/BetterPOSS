b315d1c83281fd154f431380c65d62883bc06b78
Gavin L <gavin.leung.cms@gmail.com>
2021-08-24 15:00:30 -0400

    started basic menu
    started basic menu, use mdbreact

--- client/src/components/MenuSystem/MenuSystem.js
+++ client/src/components/MenuSystem/MenuSystem.js
@@ -1,8 +1,64 @@
-import React from "react";
-import './MenuSystem.scss'
+// import React, { Component } from "react";
+// import "./MenuSystem.scss";
 
-function MenuSystem() {
-  return <div className="container__menu">Menu</div>;
+// class MenuSystem extends Component {
+  
+//   render() {
+//     return (
+//       <div className="container__menu">
+//         Menu
+        
+//       </div>
+//     );
+//   }
+// }
+
+// export default MenuSystem;
+
+import React, { useState } from 'react';
+import {
+  MDBTabs,
+  MDBTabsItem,
+  MDBTabsLink,
+  MDBTabsContent,
+  MDBTabsPane
+} from 'mdb-react-ui-kit';
+
+export default function MenuSystem() {
+  const [basicActive, setBasicActive] = useState('tab1');
+
+  const handleBasicClick = (value) => {
+    if (value === basicActive) {
+      return;
+    }
+    setBasicActive(value);
+  };
+
+  return (
+    <div>
+      <MDBTabs className='mb-3'>
+        <MDBTabsItem>
+          <MDBTabsLink onClick={() => handleBasicClick('tab1')} active={basicActive === 'tab1'}>
+            Burgers
+          </MDBTabsLink>
+        </MDBTabsItem>
+        <MDBTabsItem>
+          <MDBTabsLink onClick={() => handleBasicClick('tab2')} active={basicActive === 'tab2'}>
+            Drinks
+          </MDBTabsLink>
+        </MDBTabsItem>
+        <MDBTabsItem>
+          <MDBTabsLink onClick={() => handleBasicClick('tab3')} active={basicActive === 'tab3'}>
+            Sides
+          </MDBTabsLink>
+        </MDBTabsItem>
+      </MDBTabs>
+
+      <MDBTabsContent>
+        <MDBTabsPane show={basicActive === 'tab1'}>Tab 1 content</MDBTabsPane>
+        <MDBTabsPane show={basicActive === 'tab2'}>Tab 2 content</MDBTabsPane>
+        <MDBTabsPane show={basicActive === 'tab3'}>Tab 3 content</MDBTabsPane>
+      </MDBTabsContent>
+    </div>
+  );
 }
-
-export default MenuSystem;
--- client/src/components/OrderSummary/OrderSummary.js
+++ client/src/components/OrderSummary/OrderSummary.js
@@ -56,86 +56,6 @@ const temp = {
       special: ["PROMO"],
       amount: 1,
       price: 0.0,
-    },
-    {
-      type: "Fries",
-      name: "FFries",
-      size: "S",
-      special: ["PROMO"],
-      amount: 1,
-      price: 0.0,
-    },
-    {
-      type: "Fries",
-      name: "FFries",
-      size: "S",
-      special: ["PROMO"],
-      amount: 1,
-      price: 0.0,
-    },
-    {
-      type: "Fries",
-      name: "FFries",
-      size: "S",
-      special: ["PROMO"],
-      amount: 1,
-      price: 0.0,
-    },
-    {
-      type: "Fries",
-      name: "FFries",
-      size: "S",
-      special: ["PROMO"],
-      amount: 1,
-      price: 0.0,
-    },
-    {
-      type: "Fries",
-      name: "FFries",
-      size: "S",
-      special: ["PROMO"],
-      amount: 1,
-      price: 0.0,
-    },
-    {
-      type: "Fries",
-      name: "FFries",
-      size: "S",
-      special: ["PROMO"],
-      amount: 1,
-      price: 0.0,
-    },
-    {
-      type: "Fries",
-      name: "FFries",
-      size: "S",
-      special: ["PROMO"],
-      amount: 1,
-      price: 0.0,
-    },
-    {
-      type: "Fries",
-      name: "FFries",
-      size: "S",
-      special: ["PROMO"],
-      amount: 1,
-      price: 0.0,
-    },
-    {
-      type: "Fries",
-      name: "FFries",
-      size: "S",
-      special: ["PROMO"],
-      amount: 1,
-      price: 0.0,
-    },
-    {
-      type: "Fries",
-      name: "FFries",
-      size: "S",
-      special: ["PROMO"],
-      amount: 1,
-      price: 0.0,
     }
   ],
   subtotal: 5.58,
@@ -143,63 +63,65 @@ const temp = {
   total: 6.31,
 };
 
-const OrderSummary = () => {
-  return (
-    <div className="container__order">
-      <div>
-        <h2 style={{ textAlign: "center" }}>ORDER SUMMARY</h2>
-      </div>
-      <div className="order__title">
-        <div className="order__grids">
-          <div>Qt.</div>
-          <div>Item</div>
-          <div>Size</div>
-          <div>Price</div>
-        </div>
-      </div>
-      <div className="order__scroller">
-        <div className="order__rows">
-          {temp.items.map((item, key) => {
-            return (
-              <div>
-                <div className="order__row__columns">
-                  <div>{item.amount}</div>
-                  <div>{item.name}</div>
-                  <div>{item.size ? item.size : " "}</div>
-                  <div>{item.price.toFixed(2)}</div>
-                </div>
-                <div className="order__special">
-                  {/* {item.special ? item.special : " "} */}
-                  {item.special
-                    ? item.special.map((special) => {
-                        return (
-                          <div className="inner">
-                            <div>*{special}</div>
-                          </div>
-                        );
-                      })
-                    : " "}
-                </div>
-              </div>
-            );
-          })}
-        </div>
+class OrderSummary extends React.Component {
+  render() {
+    return (
+      <div className="container__order">
         <div>
-          <div className="order__divider">
-            *****************************************
+          <h2 style={{ textAlign: "center" }}>ORDER SUMMARY</h2>
+        </div>
+        <div className="order__title">
+          <div className="order__grids">
+            <div>Qt.</div>
+            <div>Item</div>
+            <div>Size</div>
+            <div>Price</div>
           </div>
-          <div className="order__totals">
-            <div>SubTOTAL:</div>
-            <div>{temp.subtotal.toFixed(2)}</div>
-            <div>TAX:</div>
-            <div>{temp.tax.toFixed(2)}</div>
-            <div>TOTAL:</div>
-            <div>{temp.total.toFixed(2)}</div>
+        </div>
+        <div className="order__scroller">
+          <div className="order__rows">
+            {temp.items.map((item, key) => {
+              return (
+                <div>
+                  <div className="order__row__columns">
+                    <div>{item.amount}</div>
+                    <div>{item.name}</div>
+                    <div>{item.size ? item.size : " "}</div>
+                    <div>{item.price.toFixed(2)}</div>
+                  </div>
+                  <div className="order__special">
+                    {/* {item.special ? item.special : " "} */}
+                    {item.special
+                      ? item.special.map((special) => {
+                          return (
+                            <div className="inner">
+                              <div>*{special}</div>
+                            </div>
+                          );
+                        })
+                      : " "}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <div>
+            <div className="order__divider">
+              *****************************************
+            </div>
+            <div className="order__totals">
+              <div>SubTOTAL:</div>
+              <div>{temp.subtotal.toFixed(2)}</div>
+              <div>TAX:</div>
+              <div>{temp.tax.toFixed(2)}</div>
+              <div>TOTAL:</div>
+              <div>{temp.total.toFixed(2)}</div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default OrderSummary;
